fb00a8365f46ef41bc5bf79b05e708595fa0a394
bnemos <bnamys16@gmail.com>
2022-01-27 19:29:59 +0600

    CRUD For All controllers created

---
+++ src/test/java/kg/megalab/outsourcingagreement/OutsourcingAgreementApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.megalab.outsourcingagreement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutsourcingAgreementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
